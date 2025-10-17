00932f378680b5c67f12c6133ea07fead996366f
melharbaugh <melharbaugh2@gmail.com>
2025-10-17 14:23:44 -0700

    completed sql pt3 exercises

---
+++ sql-part-3/exercises/sql_pt3_exercises.sql
@@ -0,0 +1,40 @@
+SELECT TOP 50 b.title, b.average_rating, b.books_count,tr.user_id, tr.book_id
+FROM BooksDB.dbo.books AS b
+LEFT JOIN BooksDB.dbo.to_read AS tr
+ON b.best_book_id = tr.book_id
+ORDER BY b.average_rating
+
+SELECT TOP 50 b.title, b.average_rating, b.books_count,tr.user_id, tr.book_id
+FROM BooksDB.dbo.books AS b
+LEFT JOIN BooksDB.dbo.to_read AS tr
+ON b.best_book_id = tr.book_id
+WHERE tr.user_id IS NOT NULL
+ORDER BY b.average_rating;
+
+SELECT TOP 50 b.title, b.average_rating, b.books_count, tr.user_id, tr.book_id
+FROM BooksDB.dbo.books AS b
+RIGHT JOIN BooksDB.dbo.to_read AS tr
+ON b.best_book_id = tr.book_id;
+
+-- FULL JOIN
+
+-- FULL JOIN
+SELECT TOP 30 b.title, b.average_rating, b.books_count,tr.user_id, tr.book_id
+FROM BooksDB.dbo.books AS b
+FULL JOIN BooksDB.dbo.to_read AS tr
+ON b.book_id = tr.book_id
+WHERE b.title IS NOT NULL AND tr.USER_ID IS NOT NULL
+ORDER BY b.title
+
+-- INNER JOIN
+SELECT TOP 30 b.title, b.average_rating, b.books_count,tr.user_id, tr.book_id
+FROM BooksDB.dbo.books AS b
+INNER JOIN BooksDB.dbo.to_read AS tr
+ON b.book_id = tr.book_id
+
+SELECT TOP 30 b.title, b.authors, ta.tag_id
+FROM BooksDB.dbo.books AS b
+INNER JOIN BooksDB.dbo.book_tags AS ta
+ON b.best_book_id = ta.goodreads_book_id
+ORDER BY b.title 
+
